5e78919ace04b955649d11c92fbf8298cc159f4d
Dev-May <mai.eldhrawy@gmail.com>
2026-01-26 20:41:49 +0200

    feat(db): add paginate helper and update repository find signature

--- src/DB/repositories/db.repository.ts
+++ src/DB/repositories/db.repository.ts
@@ -18,35 +18,79 @@ export abstract class DbRepository<TDocument> {
 
   async findOne(
     filter: RootFilterQuery<TDocument>,
-    select?: ProjectionType<TDocument>
+    select?: ProjectionType<TDocument>,
+    options?: QueryOptions<TDocument>,
   ): Promise<HydratedDocument<TDocument> | null> {
-    return this.model.findOne(filter, select);
+    return this.model.findOne(filter, select, options);
   }
 
-  async find(
-    filter: RootFilterQuery<TDocument>,
-    select?: ProjectionType<TDocument>,
-    options?: QueryOptions<TDocument>
-  ): Promise<HydratedDocument<TDocument>[]> {
+  async find({
+    filter,
+    select,
+    options,
+  }: {
+    filter: RootFilterQuery<TDocument>;
+    select?: ProjectionType<TDocument>;
+    options?: QueryOptions<TDocument>;
+  }): Promise<HydratedDocument<TDocument>[]> {
     return this.model.find(filter, select, options);
+  }
+
+  async paginate({
+    filter,
+    query,
+    select,
+    options,
+  }: {
+    filter: RootFilterQuery<TDocument>;
+    query: { page: number; limit: number };
+    select?: ProjectionType<TDocument>;
+    options?: QueryOptions<TDocument>;
+  }) {
+    let { page, limit } = query;
+
+    if (page < 0) page = 1;
+    page = page * 1 || 1;
+    const skip = (page - 1) * limit;
+    const finalOptions = {
+      ...options,
+      skip,
+      limit,
+    };
+
+    const count = await this.model.countDocuments({
+      deletedAt: { $exists: false },
+    });
+
+    const numberOfPages = Math.ceil(count / limit);
+
+    const docs = await this.model.find(filter, select, finalOptions);
+    return { docs, currentPage: page, countDocuments: count, numberOfPages };
   }
 
   async updateOne(
     filter: RootFilterQuery<TDocument>,
-    update: UpdateQuery<TDocument>
+    update: UpdateQuery<TDocument>,
   ): Promise<UpdateWriteOpResult> {
     return await this.model.updateOne(filter, update);
   }
 
-  async findOneAndUpdate(filter: RootFilterQuery<TDocument>, update: UpdateQuery<TDocument>, options: QueryOptions<TDocument> | null = {new: true}): Promise<HydratedDocument<TDocument> | null> {
-    return await this.model.findOneAndUpdate(filter, update, options)
+  async findOneAndUpdate(
+    filter: RootFilterQuery<TDocument>,
+    update: UpdateQuery<TDocument>,
+    options: QueryOptions<TDocument> | null = { new: true },
+  ): Promise<HydratedDocument<TDocument> | null> {
+    return await this.model.findOneAndUpdate(filter, update, options);
   }
 
   async deleteOne(filter: RootFilterQuery<TDocument>): Promise<DeleteResult> {
     return await this.model.deleteOne(filter);
   }
 
-  async findOneAndDelete(filter: RootFilterQuery<TDocument>, options?: QueryOptions<TDocument> | null): Promise<HydratedDocument<TDocument> | null> {
-    return await this.model.findOneAndDelete(filter, options)
+  async findOneAndDelete(
+    filter: RootFilterQuery<TDocument>,
+    options?: QueryOptions<TDocument> | null,
+  ): Promise<HydratedDocument<TDocument> | null> {
+    return await this.model.findOneAndDelete(filter, options);
   }
 }
